4ab3d531e0c7d02f6f8906dc299a2db24b9377a0
wuzoo <wndyd9911@gmail.com>
2023-10-28 18:42:05 +0900

    [level 0] Title: 분수의 덧셈

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,17 @@
+function solution(numer1, denom1, numer2, denom2) {
+  var answer = [];
+
+  let finaldemon = denom1 * denom2;
+  let finalnumer = numer1 * denom2 + numer2 * denom1;
+
+  let divisor = 2;
+  while (divisor <= finaldemon) {
+    if (finaldemon % divisor === 0 && finalnumer % divisor === 0) {
+      finaldemon /= divisor;
+      finalnumer /= divisor;
+    } else {
+      divisor++;
+    }
+  }
+  return [finalnumer, finaldemon];
+}
