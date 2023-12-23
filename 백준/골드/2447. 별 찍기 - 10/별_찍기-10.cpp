c821f78d15418f5e8b3d46c44fe0c55c76ff0e14
wuzoo <wndyd9911@gmail.com>
2023-12-23 16:26:30 +0900

    [Gold 5] Title: 별 찍기-10 (2020 KB, 300 ms)

---
+++ 백준/골드/2447. 별 찍기 - 10/별_찍기-10.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void printstar(int i, int j, int n){
+    if((i / n) % 3 == 1 && (j / n) % 3 == 1){
+        cout << ' ';
+    }
+    else{
+        if(n == 1){
+            cout << "*";
+        }
+        else{
+            printstar(i, j, n / 3);
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; ++i){
+        for(int j = 0; j < n; ++j){
+            printstar(i, j, n);
+        }
+        cout << '\n';
+    }
+}
