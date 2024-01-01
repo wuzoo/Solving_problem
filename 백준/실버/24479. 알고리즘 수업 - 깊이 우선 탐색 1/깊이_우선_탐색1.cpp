aa5a3932251cf682e1eba1655739a547e4057a06
wuzoo <wndyd9911@gmail.com>
2024-01-01 14:05:56 +0900

    [Silver 2] Title: 알고리즘 수업 - 깊이 우선 탐색 1 (11320 KB, 92 ms)

---
+++ 백준/실버/24479. 알고리즘 수업 - 깊이 우선 탐색 1/깊이_우선_탐색1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m, r;
+vector<int> graph[100001];
+int visited[100001];
+int result[100001];
+int cur = 1;
+
+void dfs(int ver){
+    visited[ver] = 1;
+    result[ver] = cur++;
+    
+    for(int i = 0; i < graph[ver].size(); ++i){
+        int v = graph[ver][i];
+        if(!visited[v]){
+            dfs(v);
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> m >> r;
+    for(int i = 0; i < m; ++i){
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    for(int i = 1; i <= n; ++i){
+        sort(graph[i].begin(), graph[i].end());
+    }
+
+    dfs(r);
+
+    for(int i = 1; i <= n; ++i){
+        cout << result[i] << '\n';
+    }
+
+    return 0;
+}
