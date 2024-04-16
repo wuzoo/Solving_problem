8fbbf07f7aa7bbe276d4a337d68164b7f83a8246
wuzoo <wndyd9911@gmail.com>
2024-04-16 13:37:25 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기 (twice)

--- 프로그래머스/lv2/154539 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/lv2/154539 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,21 +1,17 @@
 function solution(numbers) {
-  var answer = [-1];
+  const answer = [-1];
 
-  const st = [];
-  st.push(numbers[numbers.length - 1]);
-
+  const st = [numbers.at(-1)];
   for (let i = numbers.length - 2; i >= 0; --i) {
     let num = -1;
-
     while (st.length) {
-      if (st[0] > numbers[i]) {
-        num = st[0];
+      if (st.at(0) > numbers[i]) {
+        num = st.at(0);
         break;
       } else {
         st.shift();
       }
     }
-
     answer.push(num);
     st.unshift(numbers[i]);
   }
