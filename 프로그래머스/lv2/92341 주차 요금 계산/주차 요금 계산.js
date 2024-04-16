1d38c067408f158d45a7b84f8bedcb59a9f9f667
wuzoo <wndyd9911@gmail.com>
2024-04-16 16:05:48 +0900

    [level 2] Title: 주차 요금 계산 (twice)

--- 프로그래머스/lv2/92341 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/lv2/92341 주차 요금 계산/주차 요금 계산.js
@@ -1,50 +1,46 @@
+function getTime(time) {
+  const [h, m] = time.split(":");
+
+  return +h * 60 + +m;
+}
+
 function solution(fees, records) {
-  var answer = [];
+  const answer = [];
 
-  const [defaultTime, defaultFee, unitTime, unitFee] = fees;
-
-  function calcTime(start, end) {
-    const [hIn, mIn] = String(start)
-      .split(":")
-      .map((s) => +s);
-    const [hOut, mOut] = String(end)
-      .split(":")
-      .map((s) => +s);
-
-    return mOut - mIn + (hOut - hIn) * 60;
-  }
-
-  const startMap = new Map();
-  const endMap = new Map();
+  const timeMap = new Map();
   const feeMap = new Map();
 
-  records.forEach((item) => {
-    const [time, number, flag] = item.split(" ");
+  records.forEach((str) => {
+    const id = str.split(" ")[1];
+    timeMap.set(id, {});
+    feeMap.set(id, 0);
+  });
 
+  records.forEach((str) => {
+    const [time, id, flag] = str.split(" ");
     if (flag === "IN") {
-      startMap.set(number, time);
+      timeMap.get(id).start = time;
     } else if (flag === "OUT") {
-      endMap.set(
-        number,
-        (endMap.get(number) ?? 0) + calcTime(startMap.get(number), time)
-      );
-      startMap.delete(number);
+      timeMap.get(id).end = time;
+      const fee = getTime(timeMap.get(id).end) - getTime(timeMap.get(id).start);
+      feeMap.set(id, feeMap.get(id) + fee);
+      timeMap.set(id, {});
     }
   });
 
-  for (let [key, value] of startMap) {
-    const lastTime = "23:59";
-    endMap.set(key, (endMap.get(key) ?? 0) + calcTime(value, lastTime));
-  }
-
-  for (let [key, value] of endMap) {
-    if (value <= defaultTime) {
-      answer.push([key, defaultFee]);
-    } else {
-      const fee =
-        defaultFee + Math.ceil((value - defaultTime) / unitTime) * unitFee;
-      answer.push([key, fee]);
+  for (let [key, value] of timeMap) {
+    const { start, end } = value;
+    const lastTime = getTime("23:59");
+    if (start !== undefined && !end) {
+      feeMap.set(key, feeMap.get(key) + lastTime - getTime(start));
     }
+  }
+  for (let [key, value] of feeMap) {
+    let fee = fees[1];
+    if (value > fees[0]) {
+      fee += Math.ceil((value - fees[0]) / fees[2]) * fees[3];
+    }
+    answer.push([key, fee]);
   }
 
   return answer.sort((a, b) => +a[0] - +b[0]).map((item) => item[1]);
