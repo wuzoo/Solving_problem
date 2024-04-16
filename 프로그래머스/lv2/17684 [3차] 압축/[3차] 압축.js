cf0ae25241c850e03c291d275afb71b69bef795c
wuzoo <wndyd9911@gmail.com>
2024-04-16 13:11:09 +0900

    [level 2] Title: [3차] 압축 (twice)

--- 프로그래머스/lv2/17684 [3차] 압축/[3차] 압축.js
+++ 프로그래머스/lv2/17684 [3차] 압축/[3차] 압축.js
@@ -1,25 +1,19 @@
 function solution(msg) {
-  var answer = [];
-
-  const arr = [null];
-
-  for (let i = 65; i <= 90; ++i) {
-    arr.push(String.fromCharCode(i));
-  }
+  const answer = [];
+  const dict = new Array(27).fill(0).map((_, i) => String.fromCharCode(i + 64));
 
   let tmp = "";
   for (let i = 0; i < msg.length; ++i) {
-    if (arr.includes(tmp + msg[i])) {
+    if (dict.includes(tmp + msg[i])) {
       tmp += msg[i];
-      continue;
     } else {
-      answer.push(arr.indexOf(tmp));
-      arr.push(tmp + msg[i]);
+      answer.push(dict.indexOf(tmp));
+      dict.push(tmp + msg[i]);
       i--;
       tmp = "";
     }
   }
-  answer.push(arr.indexOf(tmp));
+  answer.push(dict.indexOf(tmp));
 
   return answer;
 }
