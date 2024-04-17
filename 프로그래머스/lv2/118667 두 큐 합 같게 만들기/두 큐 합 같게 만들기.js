9919fdc5056d9ac9bf065109ab38014fd8179096
wuzoo <wndyd9911@gmail.com>
2024-04-17 13:55:32 +0900

    [level 2] Title: 두 큐 합 같게 만들기 (twice)

--- 프로그래머스/lv2/118667 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/lv2/118667 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,29 +1,29 @@
 function solution(queue1, queue2) {
   var answer = 0;
 
-  const tmp1 = [...queue1],
-    tmp2 = [...queue2];
+  const q1len = queue1.length,
+    q2len = queue2.length;
 
-  let sum1 = tmp1.reduce((acc, cur) => acc + cur, 0);
-  let sum2 = tmp2.reduce((acc, cur) => acc + cur, 0);
+  let sum1 = queue1.reduce((acc, cur) => acc + cur, 0);
+  let sum2 = queue2.reduce((acc, cur) => acc + cur, 0);
 
   let idx1 = 0,
     idx2 = 0;
   while (sum1 !== sum2) {
-    if (idx1 >= queue1.length && idx2 >= queue2.length) {
+    if (idx1 >= q1len && idx2 >= q2len) {
       answer = -1;
       break;
     }
     if (sum1 > sum2) {
-      tmp2.push(tmp1[idx1]);
-      sum1 -= tmp1[idx1];
-      sum2 += tmp1[idx1];
-      idx1++;
-    } else {
-      tmp1.push(tmp2[idx2]);
-      sum2 -= tmp2[idx2];
-      sum1 += tmp2[idx2];
-      idx2++;
+      const abstractNum = queue1[idx1++];
+      sum1 -= abstractNum;
+      queue2.push(abstractNum);
+      sum2 += abstractNum;
+    } else if (sum1 < sum2) {
+      const abstractNum = queue2[idx2++];
+      sum2 -= abstractNum;
+      queue1.push(abstractNum);
+      sum1 += abstractNum;
     }
     answer++;
   }
