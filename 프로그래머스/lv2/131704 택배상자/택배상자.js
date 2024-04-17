0534897c3e4ed43dda3ac7c9d5ed77aa5a19d86c
wuzoo <wndyd9911@gmail.com>
2024-04-17 13:01:11 +0900

    [level 2] Title: 택배 상자 (twice)

--- 프로그래머스/lv2/131704 택배상자/택배상자.js
+++ 프로그래머스/lv2/131704 택배상자/택배상자.js
@@ -1,29 +1,22 @@
 function solution(order) {
-  const subConveyor = [];
   const truck = [];
-  const conveyor = new Array(order.length).fill(0).map((_, i) => i + 1);
+  const boxArray = new Array(order.length)
+    .fill(0)
+    .map((_, i) => i + 1)
+    .reverse();
+  const subArray = [];
 
-  let index = 0;
-  let cnt = 0;
-
-  while (conveyor.length || subConveyor.length) {
-    const current = index < conveyor.length ? conveyor[index] : -1;
-    const subCurrent = subConveyor.length
-      ? subConveyor[subConveyor.length - 1]
-      : -1;
-
-    if (current === order[cnt]) {
-      cnt++;
-      index++;
-      truck.push(current);
-    } else if (subCurrent === order[cnt]) {
-      cnt++;
-      truck.push(subCurrent);
-      subConveyor.pop();
+  let idx = 0;
+  while ((boxArray.length || subArray.length) && idx < order.length) {
+    if (boxArray.at(-1) === order[idx]) {
+      truck.push(boxArray.pop());
+      idx++;
+    } else if (subArray.at(-1) === order[idx]) {
+      truck.push(subArray.pop());
+      idx++;
     } else {
-      if (current === -1) break;
-      index++;
-      subConveyor.push(current);
+      if (!boxArray.length) break;
+      subArray.push(boxArray.pop());
     }
   }
 
