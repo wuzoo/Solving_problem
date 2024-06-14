d612662b5bedf93ce60354999adab87adc91ca42
wuzoo <wndyd9911@gmail.com>
2024-06-14 23:21:42 +0900

    [level 2] Title: 모음사전 (twice)

--- 프로그래머스/lv2/84512 모음 사전/모음사전.js
+++ 프로그래머스/lv2/84512 모음 사전/모음사전.js
@@ -1,22 +1,18 @@
 function solution(word) {
-  var answer = 0;
-
-  const arr = ["A", "E", "I", "O", "U"];
+  const dict = ["A", "E", "I", "O", "U"];
 
   const list = [];
 
-  function DFS(str, cnt) {
-    if (cnt <= 5) {
-      list.push(str);
-      for (let i = 0; i < 5; ++i) {
-        DFS(str + arr[i], cnt + 1);
-      }
+  function DFS(k, str) {
+    if (k === 6) return;
+
+    list.push(str);
+    for (let i = 0; i < 5; ++i) {
+      DFS(k + 1, str + dict[i]);
     }
   }
 
-  DFS("", 0);
+  DFS(0, "");
 
-  answer = list.indexOf(word);
-
-  return answer;
+  return list.findIndex((item) => item === word);
 }
