733afd1e64f38c00906e05741d1b2475825f80db
wuzoo <wndyd9911@gmail.com>
2024-09-09 17:10:01 +0900

    [Silver 3] Title: 정수 a를 k로 만들기 ( 51096 KB, 164 ms )

--- 백준/실버/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.js
+++ 백준/실버/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.js
@@ -3,6 +3,33 @@ const stdin = fs.readFileSync("ex.txt").toString().trim().split("\n");
 
 const [A, K] = stdin[0].split(" ").map(Number);
 
+function solution2() {
+  const visited = new Array(1000001).fill(0);
+
+  const dx = (cur) => [cur * 2, cur + 1];
+
+  const q = [[A, 0]];
+  visited[A] = 1;
+  let index = 0;
+  while (q.length > index) {
+    const [node, cnt] = q[index++];
+
+    if (node === K) {
+      console.log(cnt);
+      return;
+    }
+
+    for (const v of dx(node)) {
+      if (v < 1 || v > K) continue;
+
+      if (!visited[v]) {
+        visited[v] = 1;
+        q.push([v, cnt + 1]);
+      }
+    }
+  }
+}
+
 function solution() {
   const dp = new Array(K + 1).fill(1000001);
 
@@ -29,5 +56,7 @@ function solution() {
 
   console.log(dp[K]);
 }
+
+solution2();
 
 solution();
