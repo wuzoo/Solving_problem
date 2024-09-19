969058543927b15a01c090b2a15e871a6771dcd8
wuzoo <wndyd9911@gmail.com>
2024-09-19 19:52:48 +0900

    [Gold 5] Title: 보물섬 ( 2032 KB, 124 ms )

--- 백준/골드/2589. 보물섬/보물섬.cpp
+++ 백준/골드/2589. 보물섬/보물섬.cpp
@@ -1,7 +1,70 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <cstring>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+int N, M;
+char map[51][51];
+int visited[51][51];
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {1, -1, 0, 0};
+int ans;
+
+void BFS(int x, int y){
+    queue<pair<int, int>> q;
+    q.push({x, y});
+    visited[x][y] = 1;
+    
+    while(q.size()){
+        int x = q.front().first;
+        int y = q.front().second;
+
+        q.pop();
+
+        for(int i = 0; i < 4; ++i){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || nx >= M || ny < 0 || ny >= N || map[nx][ny] == 'W') continue;
+
+            if(visited[nx][ny]) continue;
+
+            visited[nx][ny] = visited[x][y] +  1;
+            q.push({nx, ny});
+
+            if(ans < visited[nx][ny]){
+                ans = visited[nx][ny];
+            }
+
+            
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> M >> N;;
+
+    for(int i = 0; i < M; ++i){
+        for(int j = 0; j < N; ++j){
+            char c;
+            cin >> c;
+            map[i][j] = c;
+        }
+    }
+
+    for(int i = 0; i < M; ++i){
+        for(int j = 0; j < N; ++j){
+            if(map[i][j] == 'L'){
+                BFS(i, j);
+                memset(visited, 0, sizeof(visited));
+            }
+        }
+    }
+
+    cout << ans - 1;
 }
