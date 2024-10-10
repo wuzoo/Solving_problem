c62752c17df152b7dd5bba7f87614ab675c5bcf0
wuzoo <wndyd9911@gmail.com>
2024-10-10 17:32:48 +0900

    [Gold 2] Title: 가장 긴 증가하는 부분 수열 3

---
+++ 백준/골드/12738. 가장 긴 증가하는 부분 수열 3/12738.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[1000001];
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; ++i){
+        cin >> arr[i];
+    }
+    
+    vector<int> v;
+
+    for(int i = 0; i < n; ++i){
+        if(v.empty() || v.back() < arr[i]){
+            v.push_back(arr[i]);
+        }else{
+            int idx = lower_bound(v.begin(), v.end(), arr[i]) - v.begin();
+
+            v[idx] = arr[i];
+        }
+    }
+
+    cout << v.size();
+}
